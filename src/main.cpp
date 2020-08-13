c6a917b25ba707e30072b4e661ae4f068f02c475
sijah.ak <sijah.ak@gmail.com>
2020-08-13 21:47:29 +0530

    html rgb

--- src/main.cpp
+++ src/main.cpp
@@ -9,40 +9,40 @@ uint8_t led1 = D1;
 uint8_t led2 = D2;
 uint8_t led3 = D3;
 
-const char *ssid="o2";
-const char *pass="asdfghjkl";
+const char *ssid="CABIN1";//CABIN1 //o2
+const char *pass="asdfghjkl";//asdfghjkl
 
 void handle_root() {
-    File p = LittleFS.open("/index.html","r");
+    File p = LittleFS.open("/index.html", "r");
     server.streamFile(p, "text/html");
 
 }
 void handle_notfound()
 {
 
-  server.send(404,"text/html","jango njan pettu");
+    server.send(404, "text/html", "jango njan pettu");
 }
 
-void handle_rgb(){
-String red = server.arg("r");
-  String green = server.arg("g");
-  String blue = server.arg("b");
-  int r = red.toInt();
-  int g = green.toInt();
-  int b = blue.toInt();
- 
-  Serial.print("Red:");Serial.println(r);
-  Serial.print("Green:");Serial.println(g);
-  Serial.print("Blue:");Serial.println(b);
+void handle_rgb() {
+    String red = server.arg("r");
+    String green = server.arg("g");
+    String blue = server.arg("b");
+    int r = red.toInt();
+    int g = green.toInt();
+    int b = blue.toInt();
 
-server.send(200,"text/plane",red);
+    Serial.print("Red:");Serial.println(r);
+    Serial.print("Green:");Serial.println(g);
+    Serial.print("Blue:");Serial.println(b);
 
-int r1 =map(r,0,100,0,1024);
-int g1 =map(g,0,100,0,1024);
-int b1 =map(b,0,100,0,1024);
-analogWrite(led3,r1);
-analogWrite(led2,g1);
-analogWrite(led1,b1);
+    server.send(200, "text/plane", red);
+
+    int r1 =map(r, 0, 100, 0, 1024);
+    int g1 =map(g, 0, 100, 0, 1024);
+    int b1 =map(b, 0, 100, 0, 1024);
+    analogWrite(led3, r1);
+    analogWrite(led2, g1);
+    analogWrite(led1, b1);
 }
 
 
@@ -53,21 +53,21 @@ void setup() {
     pinMode(led2, OUTPUT);
     pinMode(led3, OUTPUT);
 
-    if(LittleFS.begin())
- {
-     Serial.print("littlefson");
- }
- else
- {
-     Serial.print("littlefs no working");
- }
+    if (LittleFS.begin())
+    {
+        Serial.print("littlefson");
+    }
+    else
+    {
+        Serial.print("littlefs no working");
+    }
     WiFi.begin(ssid, pass);
     while (WiFi.status() != WL_CONNECTED)
     {
         Serial.print(".");
         delay(1000);
     }
-    
+
     if (!MDNS.begin("esp8266"))
     {
         Serial.println("Error setting up MDNS responder!");
@@ -77,9 +77,9 @@ void setup() {
         }
     }
     server.on("/", handle_root);
-    server.on("/RGB",handle_rgb);
+    server.on("/RGB", handle_rgb);
     server.onNotFound(handle_notfound);
-server.begin();
+    server.begin();
 
 
 
